7ada7ad3b661454431b1b450f5aa22b0842cb38f
zeroAska <trotwoodray@gmail.com>
2014-04-12 23:49:23 -0700

    Add some address edit to the DetailContact...

--- lahack/lahack/ContactDetailViewController.h
+++ lahack/lahack/ContactDetailViewController.h
@@ -9,5 +9,16 @@
 #import <UIKit/UIKit.h>
 
 @interface ContactDetailViewController : UIViewController
-    @property (strong, nonatomic) NSArray *ContactDetailModel;
+
+@property (strong, nonatomic) NSArray *ContactDetailModel;
+@property (weak, nonatomic) IBOutlet UIImageView *ContactPhoto;
+@property (weak, nonatomic) IBOutlet UITextView *PhoneNumber;
+@property (weak, nonatomic) IBOutlet UITextView *FirstName;
+@property (weak, nonatomic) IBOutlet UITextView *LastName;
+@property (weak, nonatomic) IBOutlet UINavigationBar *FullName;
+@property (weak, nonatomic) IBOutlet UITextView *EmailAddress;
+@property (weak, nonatomic) IBOutlet UITextView *Company;
+@property (weak, nonatomic) IBOutlet UITextView *Position;
+@property (weak, nonatomic) IBOutlet UISwitch *isUpdated;
+
 @end
--- lahack/lahack/ContactDetailViewController.m
+++ lahack/lahack/ContactDetailViewController.m
@@ -27,6 +27,16 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    _PhoneNumber.text = _ContactDetailModel[1];
+    _FirstName.text = _ContactDetailModel[2];
+     _LastName.text = _ContactDetailModel[3];
+     _FirstName.text = _ContactDetailModel[4];
+     _FullName.text = _ContactDetailModel[5];
+     _EmailAddress.text = _ContactDetailModel[6];
+     _Company.text = _ContactDetailModel[7];
+     _Position.text = _ContactDetailModel[8];
+     _isUpdated.on = _ContactDetailModel[9];
+    _ContactPhoto.image = [UIImage imageNamed:_ContactDetailModel[0]];
 }
 
 - (void)didReceiveMemoryWarning
