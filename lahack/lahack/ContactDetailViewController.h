0f3bf12b40bdc3b073957a2a7975833bdd4e5335
zeroAska <trotwoodray@gmail.com>
2014-04-12 21:07:57 -0700

    Add the ContactDetailViewController
    Lack the .m implementations

---
+++ lahack/lahack/ContactDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactDetailViewController.h
+//  lahack
+//
+//  Created by Yixin Cai on 4/12/14.
+//  Copyright (c) 2014 Ray Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactDetailViewController : UIViewController
+    @property (strong, nonatomic) NSArray *ContactDetailModel;
+@end
---
+++ lahack/lahack/ContactDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  ContactDetailViewController.m
+//  lahack
+//
+//  Created by Yixin Cai on 4/12/14.
+//  Copyright (c) 2014 Ray Zhang. All rights reserved.
+//
+
+#import "ContactDetailViewController.h"
+
+@interface ContactDetailViewController ()
+
+@end
+
+@implementation ContactDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
