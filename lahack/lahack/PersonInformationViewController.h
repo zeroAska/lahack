c682a9df4dd9e9cbebb412c774430862cf49eb37
Yixin Cai <yixincai@usc.edu>
2014-04-12 23:45:14 -0700

    add person info update

---
+++ lahack/lahack/PersonInformationViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersonInformationViewController.h
+//  lahack
+//
+//  Created by Yixin Cai on 4/12/14.
+//  Copyright (c) 2014 Ray Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonInformationViewController : UIViewController
+
+@end
---
+++ lahack/lahack/PersonInformationViewController.m
@@ -0,0 +1,56 @@
+//
+//  PersonInformationViewController.m
+//  lahack
+//
+//  Created by Yixin Cai on 4/12/14.
+//  Copyright (c) 2014 Ray Zhang. All rights reserved.
+//
+
+#import "PersonInformationViewController.h"
+
+@interface PersonInformationViewController ()
+
+@end
+
+@implementation PersonInformationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+- (IBAction)updatePersonInfo:(id)sender {
+}
+
+- (IBAction)cancelUpdateInfo:(id)sender {
+}
+
+
+
+@end
